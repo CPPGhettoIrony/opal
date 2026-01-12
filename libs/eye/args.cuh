02825859e622b68e6cf68c7ef50bbe2f5ec9ed22
skarse <50168777+CPPGhettoIrony@users.noreply.github.com>
2026-01-12 18:37:24 +0100

    i give up

--- libs/eye/args.cuh
+++ libs/eye/args.cuh
@@ -15,7 +15,12 @@ struct Args {
     vec3 eye_dim    = vec3(0.08, 0.073, 0.028);
     vec3 eye_pos    = vec3(.0, .0, 0.018);
     mat3 eye_rot    = mat3(1.f);
-    
+
+    vec3 eyeline_dim_offset = vec3(.0);
+    mat3 eyeline_rot_offset = mat3(1.);
+    float eyeline_separation_offset = 0;
+    float eyeline_angle_offset = 0;
+
     float eyeline_len = 0.022f;
     float eyeline_rad = 0.79;
     float eyeline_thk = 0.94;
@@ -31,9 +36,9 @@ DECLARE_WINDOW(eyeControls, 10, 10, (WINDOW_WIDTH + 50)*2, 800) {
     CALL_GUI(dualEyeControlGUI, WINDOW_WIDTH + 50, a.eye1, a.eye2, a.eye_mode)
 }
 
-DECLARE_WINDOW(eyeAdjustments, 300, 10, WINDOW_WIDTH + 160, 1000) {
+DECLARE_WINDOW(eyeAdjustments, 300, 10, WINDOW_WIDTH + 160, 1200) {
 
-    static vec3 rot(.0);
+    static vec3 rot(.0), e_rot(.0);
 
     ADD_LABEL("Eye Spacing Controls")
     ADD_SEPARATOR(WINDOW_WIDTH);
@@ -43,7 +48,7 @@ DECLARE_WINDOW(eyeAdjustments, 300, 10, WINDOW_WIDTH + 160, 1000) {
     ADD_LABEL("\t Eyes Angle")
     ADD_SLIDER(WINDOW_WIDTH, 0, 3.14, a.eyes_angle);
 
-    ADD_LABEL("Other Properties")
+    ADD_LABEL("Transform")
     ADD_SEPARATOR(WINDOW_WIDTH)
 
     ADD_LABEL("\t Dimensions")
@@ -52,6 +57,10 @@ DECLARE_WINDOW(eyeAdjustments, 300, 10, WINDOW_WIDTH + 160, 1000) {
     ADD_VEC3_SLIDER(WINDOW_WIDTH, -0.2, 0.2, a.eye_pos)
     ADD_LABEL("\t Rotation")
     ADD_VEC3_SLIDER(WINDOW_WIDTH, -6.28, 6.28, rot)
+
+    ADD_LABEL("Eyeline Properties")
+    ADD_SEPARATOR(WINDOW_WIDTH)
+
     ADD_LABEL("\t Eyeline Length")
     ADD_SLIDER(WINDOW_WIDTH, 0, 1, a.eyeline_len)
     ADD_LABEL("\t Eyeline Thickness")
@@ -59,9 +68,22 @@ DECLARE_WINDOW(eyeAdjustments, 300, 10, WINDOW_WIDTH + 160, 1000) {
     ADD_LABEL("\t Eyeline Radius")
     ADD_SLIDER(WINDOW_WIDTH, 0, 1, a.eyeline_rad)
     ADD_LABEL("\t Eyeline Offset")
-    ADD_SLIDER(WINDOW_WIDTH, -0.1, 0.1, a.eyeline_off)    
+    ADD_SLIDER(WINDOW_WIDTH, -0.1, 0.1, a.eyeline_off) 
+    
+    ADD_LABEL("Eyeline Adjustments")
+    ADD_SEPARATOR(WINDOW_WIDTH)
+
+    ADD_LABEL("\t Eyeline Dimension Offset")
+    ADD_VEC3_SLIDER(WINDOW_WIDTH, 0, 1, a.eyeline_dim_offset)
+    ADD_LABEL("\t Eyeline Rotation Offset")
+    ADD_VEC3_SLIDER(WINDOW_WIDTH, -6.28, 6.28, e_rot)
+    ADD_LABEL("\t Eyeline Separation Offset")
+    ADD_SLIDER(WINDOW_WIDTH, -1, 1, a.eyeline_separation_offset)
+    ADD_LABEL("\t Eyeline Angle Offset")
+    ADD_SLIDER(WINDOW_WIDTH, -1, 1, a.eyeline_angle_offset)
 
     a.eye_rot = rotationFromEuler(rot);
+    a.eyeline_rot_offset = rotationFromEuler(e_rot);
 
 }
 
--- libs/eye/scene.cuh
+++ libs/eye/scene.cuh
@@ -19,9 +19,15 @@ Hit scene(vec3 p, vec3 n, Args args){
     head_hit = eyes(p, head_hit, args.eye_pos, args.eye_rot, args.eye_dim, args.eyes_separation, args.eyes_angle, args.eye1, args.eye2, 
                     n, EYE_A, EYE_B, SKIN, args);
 
-    Hit eyeline_hit = eyeLines(p, args.eye_pos, args.eye_rot, args.eye_dim, args.eyes_separation, args.eyes_angle, args.eyeline_rad, 
+    Hit eyeline_hit = eyeLines(
+        p, args.eye_pos, args.eye_rot * args.eyeline_rot_offset, 
+        args.eye_dim + args.eyeline_dim_offset, 
+        args.eyes_separation + args.eyeline_separation_offset, 
+        args.eyes_angle + args.eyeline_angle_offset, 
+        args.eyeline_rad, 
         args.eyeline_thk, args.eyeline_len, 
-        args.eyeline_off, args.eye1, args.eye2, n, MASC, args);
+        args.eyeline_off, args.eye1, args.eye2, n, MASC, args
+    );
 
     return join(head_hit, eyeline_hit);
 
